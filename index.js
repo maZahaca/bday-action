f767c257baa0a5056c04aaef18dfbfde1b0acd4b
Andrew Red <andrew@reddikh.com>
2022-01-24 14:17:04 +0300

    - init

---
+++ index.js
@@ -0,0 +1,36 @@
+const chalkAnimation = require('chalk-animation');
+const figlet = require('figlet');
+
+const {
+  INPUT_NAME,
+  INPUT_SECONDS_TO_STOP = '60',
+  GITHUB_ACTOR,
+} = process.env;
+
+const displayName = INPUT_NAME || GITHUB_ACTOR;
+
+const message = `Happy Birthday, ${displayName}!`;
+// Increase the spacing between words in the message.
+const formattedMessage = message.replace(/ /g, '   ');
+
+(async () => {
+  try {
+    const data = await new Promise((resolve, reject) => {
+      figlet(formattedMessage, function (err, data) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(data);
+      });
+    });
+    const animation = chalkAnimation.rainbow(data);
+    animation.start();
+
+    setTimeout(() => {
+      animation.stop();
+    }, parseInt(INPUT_SECONDS_TO_STOP, 10) * 1000)
+  } catch (err) {
+    console.log('Something went wrong ...');
+    console.dir(err);
+  }
+})();
